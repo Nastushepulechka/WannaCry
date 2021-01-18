26164a02c4a23a5621ca41ebb811b7593e526b46
Nastya <nastya.voronin2010@yandex.ru>
2021-01-18 23:05:09 +0300

    static настроила

--- ToDoL/settings.py
+++ ToDoL/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -29,7 +29,6 @@ ALLOWED_HOSTS = [
     '127.0.0.1',
     'tasks.std-712.ist.mospolytech.ru'
 ]
-
 
 # Application definition
 
@@ -141,3 +140,4 @@ IMPORT_EXPORT_USE_TRANSACTIONS = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
